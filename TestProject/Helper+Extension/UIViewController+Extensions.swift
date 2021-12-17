774d6c3bcf5fb92c76ed8de091dc2db872af0c53
nikul <nikul@oneclickdrive.com>
2021-12-17 08:42:21 +0530

    Code Update

--- TestProject/Helper+Extension/UIViewController+Extensions.swift
+++ TestProject/Helper+Extension/UIViewController+Extensions.swift
@@ -9,7 +9,7 @@ import UIKit
 
 extension UIViewController {
     func showAlert( _ message: String ) {
-        let alert = UIAlertController(title: "", message: message, preferredStyle: .alert)
+        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
         alert.addAction( UIAlertAction(title: "Ok", style: .cancel, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
